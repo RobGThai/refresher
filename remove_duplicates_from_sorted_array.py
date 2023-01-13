d8dc31b797871280ae7a2dc246538bd700e89059
Poohdish Rattanavijai <thisisrobg@gmail.com>
2023-01-13 14:47:36 +0800

    Simple loop duplication check with O(1) space

---
+++ remove_duplicates_from_sorted_array.py
@@ -0,0 +1,15 @@
+class Solution:
+    """
+    https://leetcode.com/problems/remove-duplicates-from-sorted-array
+    """
+    def removeDuplicates(self, nums: List[int]) -> int:
+        curr = nums[0]
+        i = 1
+        while i < len(nums):
+            if nums[i] <= curr:
+                nums.pop(i)
+            else:
+                curr = nums[i]
+                i += 1
+
+        return i
